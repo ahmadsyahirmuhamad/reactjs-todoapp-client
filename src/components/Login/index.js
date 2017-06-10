2029c1524067895b31bb1713a06ee4f9e0eb0413
ahmadsyahirmuhamad <ahmadsyahirmuhamad@gmail.com>
2017-06-11 05:42:12 +0800

    add login component

---
+++ src/components/Login/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  componentDidMount(){
+  }
+
+  render() {
+    return (
+      <div className="Login">
+        <h1>Login</h1>
+      </div>
+    );
+  }
+}
+
+export default Login
--- src/components/Routes/index.js
+++ src/components/Routes/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom'
 import Home from '../Home';
 import Foo from '../Foo';
+import Login from '../Login';
 
 class Routes extends Component {
   componentDidMount(){
@@ -12,6 +13,7 @@ class Routes extends Component {
       <div className="Routes">
           <Route exact path = "/" component = {Home} />
           <Route path = "/foo" component = {Foo} />
+          <Route path = "/login" component = {Login} />
       </div>
     );
   }
